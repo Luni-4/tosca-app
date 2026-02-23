5f0f2341f8ea667c45c6cda5a9dbcad90b54b3f7
Luni-4 <luni-4@hotmail.it>
2026-02-23 14:13:27 +0100

    Improve the error page
    - Distinguish between 404 and 500 errors
    - Enhance the 500 error page by adding guidance on where users
    should report the issue
    - Clean up the code by removing unused translation strings

--- src/error.rs
+++ src/error.rs
@@ -12,29 +12,43 @@ use serde::Serialize;
 
 use crate::AppState;
 
+const EMAIL_ERROR: &str = "<a href=\"mailto:error@toscalabs.org\">error@toscalabs.org</a>";
+
 #[derive(Serialize)]
 struct RenderError<'a> {
+    error_status: u16,
+    title: Cow<'static, str>,
+    email: Cow<'static, str>,
+    copy_button_alt: Cow<'static, str>,
     description: &'a str,
     info: Option<String>,
-    goto_message: Cow<'static, str>,
     index: &'static str,
+    goto_message: Cow<'static, str>,
 }
 
 impl<'a> RenderError<'a> {
     fn description(description: &'a str) -> Self {
-        Self::new(description, None)
+        Self::new(description, None, 500)
+    }
+
+    fn page_not_found(description: &'a str) -> Self {
+        Self::new(description, None, 404)
     }
 
     fn error(description: &'a str, info: String) -> Self {
-        Self::new(description, Some(info))
+        Self::new(description, Some(info), 500)
     }
 
-    fn new(description: &'a str, info: Option<String>) -> Self {
+    fn new(description: &'a str, info: Option<String>, error_status: u16) -> Self {
         Self {
+            error_status,
+            title: t!("error.title", status = error_status),
+            email: t!("error.report_message", email = EMAIL_ERROR),
+            copy_button_alt: t!("error.copy_button_alt"),
             description,
             info,
-            goto_message: t!("labels.go_to_devices"),
             index: "/",
+            goto_message: t!("error.goto_home"),
         }
     }
 }
@@ -46,28 +60,22 @@ pub(crate) fn error_with_info<T, E: std::error::Error>(
 ) -> Result<T, Error> {
     res.map_err(|e| {
         #[cfg(feature = "logging")]
-        tracing::error!("{} ~> {e}", t!("logging.request_error"));
+        tracing::error!("{}", t!("error.request_error", error = e));
         Error::error_page(env, description, e)
     })
 }
 
 pub(crate) async fn missing_assets() -> Error {
     #[cfg(feature = "logging")]
-    tracing::error!(
-        "{} ~> Error in loading the `assets` directory",
-        t!("logging.request_error"),
-    );
-    Error::new(
-        ErrorState::Assets,
-        "Error in loading the `assets` directory",
-    )
+    tracing::error!("{}", t!("error.assets_error"));
+    Error::new(ErrorState::Assets, t!("error.assets_error"), true)
 }
 
 pub(crate) async fn missing_route(State(state): State<AppState>, uri: Uri) -> Error {
-    let error = format!("{} `{uri}`", t!("labels.missing_route"));
+    let error = t!("error.missing_route", route = uri);
     #[cfg(feature = "logging")]
-    tracing::error!("{} ~> {error}", t!("logging.request_error"));
-    Error::description_page(&state.env, &error)
+    tracing::error!("{error}");
+    Error::render_template(&state.env, RenderError::page_not_found(&error))
 }
 
 #[derive(Serialize)]
@@ -105,6 +113,7 @@ enum ErrorState {
 pub(crate) struct Error {
     state: ErrorState,
     data: Cow<'static, str>,
+    is_error_500: bool,
 }
 
 impl Error {
@@ -121,23 +130,26 @@ impl Error {
     }
 
     fn render_template(env: &Environment<'static>, context: RenderError) -> Self {
+        let is_error_500 = context.error_status == 500;
+
         let template = match env.get_template("error") {
             Ok(template) => template,
-            Err(e) => return Self::new(ErrorState::Template, e.to_string()),
+            Err(e) => return Self::new(ErrorState::Template, e.to_string(), true),
         };
 
         let rendered = match template.render(context) {
             Ok(rendered) => rendered,
-            Err(e) => return Self::new(ErrorState::Render, e.to_string()),
+            Err(e) => return Self::new(ErrorState::Render, e.to_string(), true),
         };
 
-        Self::new(ErrorState::Success, rendered)
+        Self::new(ErrorState::Success, rendered, is_error_500)
     }
 
-    fn new(state: ErrorState, data: impl Into<Cow<'static, str>>) -> Self {
+    fn new(state: ErrorState, data: impl Into<Cow<'static, str>>, is_error_500: bool) -> Self {
         Self {
             state,
             data: data.into(),
+            is_error_500,
         }
     }
 }
@@ -159,6 +171,12 @@ impl IntoResponse for Error {
             .into_response(),
         };
 
-        (StatusCode::INTERNAL_SERVER_ERROR, body).into_response()
+        let status_code = if self.is_error_500 {
+            StatusCode::INTERNAL_SERVER_ERROR
+        } else {
+            StatusCode::NOT_FOUND
+        };
+
+        (status_code, body).into_response()
     }
 }
